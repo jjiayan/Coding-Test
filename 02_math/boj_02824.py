5b465807fcc2d2af64e64242bbbaa80f8c9adb41
chingudl7 <chingudl7@naver.com>
2026-04-13 15:44:58 +0900

    스택 기초

--- 02_math/boj_02824.py
+++ 02_math/boj_02824.py
@@ -24,13 +24,12 @@ def solve():
             total_gcd *= g
             if total_gcd >= 1000000000:
                 is_over = True
-                total_gcd %= 1000000000
-            ms[j] //= g
-            a //= g
+                total_gcd %= 1000000000 # 10억 이상이면 하위 9자리만 보관
+            ms[j] //= g # ms[j]에서 공약수 제거 (중복 계산 방지)
+            a //= g     # a에서도 공약수 제거
             if a == 1:
                 break
         i += 1
-            
             
     if is_over:
         print(f"{total_gcd:09d}")
